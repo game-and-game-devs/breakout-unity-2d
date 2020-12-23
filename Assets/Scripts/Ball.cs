abbc60292f3e0a86cc4522ed40d741b347069b6f
Anartz Mugika Ledo <mugan86@gmail.com>
2020-12-23 21:10:49 +0100

    Add ball actions to move up and add with prefab vertical limits

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    // Mantiene como variable privada dentro de la clase pero podemos
+    // modificar dentro del Inspector
+    [SerializeField] Rigidbody2D _rigidbody2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // _rigidbody2D = GetComponent<Rigidbody2D>();
+        _rigidbody2D.velocity = Vector2.up;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
