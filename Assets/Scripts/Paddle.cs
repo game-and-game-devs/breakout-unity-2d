fc67adcb8de1532738fef7d6250ebfdceca0741b
Anartz Mugika Ledo <mugan86@gmail.com>
2020-12-23 14:58:27 +0100

    First commit - Page 132 - Paddle game Object implement basic options

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    float speed = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Empezando a trabajar en el script del 'Paddle'");
+        Debug.Log($"Posición inicial {transform.position}");
+        // transform.position += new Vector3(-1, 0, 0);
+        transform.position += Vector3.right;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Debug.Log("Estoy en el método Update actualizándome una y otra vez");
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            // Añadir Time.deltaTime para ajustar la velocidad de desplazamiento
+            transform.position += Time.deltaTime * Vector3.right * speed;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Time.deltaTime * Vector3.left * speed;
+        }
+    }
+}
