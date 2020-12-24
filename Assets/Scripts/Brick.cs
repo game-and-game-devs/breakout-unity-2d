d59f874fb576d23d0d4676b96da6f27b4fd339b6
Anartz Mugika Ledo <mugan86@gmail.com>
2020-12-24 10:22:35 +0100

    Create Brick and define collision to destroy info

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Brick : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Al recibir la colision de la bola, se destroye el bloque "Brick"
+        Destroy(gameObject);
+    }
+}
