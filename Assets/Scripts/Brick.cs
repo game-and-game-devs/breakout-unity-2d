51af74c295dfe47a1915e9b12c454fc753b233b2
Anartz Mugika Ledo <mugan86@gmail.com>
2020-12-24 11:08:12 +0100

    Add GameManager to control BricksOnLevel

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -2,10 +2,28 @@
 
 public class Brick : MonoBehaviour
 {
+    GameObject gameManagerObject;
+    GameManager gameManager;
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Buscamos el script "GameManager" para comunicarnos con el
+        // Lo asignamos en un GameObject para tener acceso a sus componentes
+        gameManagerObject = GameObject.Find("GameManager");
+
+        // Añadimos la comprobación para asegurarnos que cargamos correctamente el
+        // el gameManager Object
+        if (gameManagerObject == null)
+        {
+            Debug.Log("Objecto no encontrado");
+        }
+        else
+        {
+            // Nos permite acceder al GameManager y a su información
+            gameManager = gameManagerObject.GetComponent<GameManager>();
+            // Reducir el número de bloques a destruir en el nivel
+            gameManager.bricksOnLevel++;
+        }
     }
 
     // Update is called once per frame
@@ -15,6 +33,24 @@ public class Brick : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        // Buscamos el script "GameManager" para comunicarnos con el
+        // Lo asignamos en un GameObject para tener acceso a sus componentes
+        gameManagerObject = GameObject.Find("GameManager");
+
+        // Añadimos la comprobación para asegurarnos que cargamos correctamente el
+        // el gameManager Object
+        if (gameManagerObject == null)
+        {
+            Debug.Log("Objecto no encontrado");
+        }
+        else
+        {
+            // Nos permite acceder al GameManager y a su información
+            gameManager = gameManagerObject.GetComponent<GameManager>();
+            // Reducir el número de bloques a destruir en el nivel
+            gameManager.bricksOnLevel--;
+        }
+        
         // Al recibir la colision de la bola, se destroye el bloque "Brick"
         Destroy(gameObject);
     }
