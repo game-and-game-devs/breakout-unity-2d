75011a678069859e35bee8661d003502d4f24dcc
Anartz Mugika Ledo <mugan86@gmail.com>
2020-12-26 11:20:19 +0100

    Load Game Scene to restart game

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-
+using UnityEngine.SceneManagement;
 
 public class UIController : MonoBehaviour
 {
@@ -7,8 +7,13 @@ public class UIController : MonoBehaviour
     [SerializeField] GameObject _winnerPanel;
     public void ActivePanel(FinishGameStateEnum optionSelect)
     {
-        bool winActiveScreen = optionSelect.Equals("WIN");
-        _losePanel.SetActive(!winActiveScreen);
-        _winnerPanel.SetActive(winActiveScreen);
+        _losePanel.SetActive(optionSelect.Equals("LOSE"));
+        _winnerPanel.SetActive(optionSelect.Equals("WIN"));
+    }
+
+    public void Restart()
+    {
+        Debug.Log("Reiniciando el juego");
+        SceneManager.LoadScene("Game");
     }
 }
