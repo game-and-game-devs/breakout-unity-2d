3b0199422e97538eeef89abb4038495bcb770b97
Anartz Mugika Ledo <mugan86@gmail.com>
2020-12-27 10:50:01 +0100

    Create MainMenuController to load game scene and add first option MainMenu

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenuController : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
